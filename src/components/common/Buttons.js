e43d76a092aee9a55abc4caf7157b0cd8913416a
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-07-17 12:35:18 +0200

    update button colors

--- src/components/common/Buttons.js
+++ src/components/common/Buttons.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const PrimaryButtonMedium = ({ href, icon: Icon, text }) => (
   <Link
     href={href}
-    className="block rounded-full min-w-[100px] border-2 border-sky-400 bg-sky-400 px-2.5 py-1.5 text-base text-center font-semibold text-slate-800 shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+    className="block rounded-full min-w-[100px] border-2 border-sky-800 bg-sky-800 px-2.5 py-1.5 text-base text-center font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
   >
     <span className="flex justify-center items-center">
       {Icon && (
@@ -39,7 +39,7 @@ export const WhiteButtonMedium = ({ href, icon: Icon, text }) => (
 export const LightButtonMedium = ({ href, icon: Icon, text }) => (
   <Link
     href={href}
-    className="block rounded-full min-w-[100px] border-2 border-sky-400 bg-transparent px-2.5 py-1.5 text-base text-center font-semibold text-sky-400 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+    className="block rounded-full min-w-[100px] border-2 border-sky-800 bg-transparent px-2.5 py-1.5 text-base text-center font-semibold text-sky-800 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
   >
     <span className="flex justify-center items-center">
       {Icon && (
@@ -75,7 +75,7 @@ export const OutlineWhiteButtonMedium = ({ href, icon: Icon, text }) => (
 export const PrimaryButtonSmall = ({ href, icon: Icon, text }) => (
   <Link
     href={href}
-    className="block rounded-full border-2 border-sky-400 bg-sky-400 px-2 py-0.5 text-base text-center font-semibold text-slate-800 shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+    className="block rounded-full border-2 border-sky-800 bg-sky-800 px-2 py-0.5 text-base text-center font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
   >
     <span className="flex justify-center items-center">
       {Icon && (
@@ -111,7 +111,7 @@ export const WhiteButtonSmall = ({ href, icon: Icon, text }) => (
 export const LightButtonSmall = ({ href, icon: Icon, text }) => (
   <Link
     href={href}
-    className="block rounded-full border-2 border-sky-400 bg-transparent px-2 py-0.5 text-base text-center font-semibold text-sky-400 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+    className="block rounded-full border-2 border-sky-800 bg-transparent px-2 py-0.5 text-base text-center font-semibold text-sky-800 shadow-sm hover:bg-sky-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
   >
     <span className="flex justify-center items-center">
       {Icon && (
@@ -148,7 +148,7 @@ export const SelectionButtonSmall = ({ icon: Icon, text, selected, onClick }) =>
   <button
     onClick={onClick}
     className={`block rounded-full border-2 px-2 text-sm text-center font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-800 ${
-      selected ? "border-sky-400 bg-sky-400 text-slate-800 hover:bg-sky-500" : "border-slate-100 bg-slate-100 text-slate-800 hover:bg-slate-200"
+      selected ? "border-sky-800 bg-sky-800 text-white hover:bg-sky-700" : "border-slate-100 bg-slate-100 text-slate-800 hover:bg-slate-200"
     }`}
   >
     <span className="flex justify-center items-center">
