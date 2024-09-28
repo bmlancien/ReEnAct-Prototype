0b12f2a6f79a92f5ff815b6f15cfb58d9dde89c6
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-09-28 12:00:28 +0100

    pass slider values to results page

--- src/app/questionsCustom/question4/page.js
+++ src/app/questionsCustom/question4/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { useSearchParams } from 'next/navigation';
 import BottomNav from "../../../components/common/BottomNav";
 import Breadcrumb from "../../../components/common/Breadcrumb";
 import LayoutSimple from "../../../components/layout/LayoutSimple";
@@ -6,6 +9,11 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import { LinkIconBlue } from "../../../components/common/Links"
 
 export default function Question4() {
+  const searchParams = useSearchParams(); // Use useSearchParams to get URL parameters
+  const totalEnergy = searchParams.get('totalEnergy') || 'N/A';
+  const windEnergy = searchParams.get('windEnergy') || 'N/A';
+  const pvEnergy = searchParams.get('pvEnergy') || 'N/A';
+
   return (
     <>
       <Breadcrumb
@@ -28,7 +36,7 @@ export default function Question4() {
             <div className="w-full md:w-1/6 px-2 mb-4 md:mb-0 flex">
               <div className="bg-white p-6 border rounded-lg shadow flex flex-col w-full">
                 <h3 className="text-lg font-medium flex-grow">Gesamtenergie&nbsp;-produktion</h3>
-                <p className="text-3xl font-bold">32,8 GWh</p>
+                <p className="text-3xl font-bold">{totalEnergy} GWh</p>
                 <p className="text-sm">+8,3%</p>
               </div>
             </div>
@@ -42,14 +50,14 @@ export default function Question4() {
             <div className="w-full md:w-1/6 px-2 mb-4 md:mb-0 flex">
               <div className="bg-white p-6 border rounded-lg shadow flex flex-col w-full">
                 <h3 className="text-lg font-medium flex-grow">Windkraft</h3>
-                <p className="text-3xl font-bold">15,0 GWh</p>
+                <p className="text-3xl font-bold">{windEnergy} GWh</p>
                 <p className="text-sm">+200%</p>
               </div>
             </div>
             <div className="w-full md:w-1/6 px-2 mb-4 md:mb-0 flex">
               <div className="bg-white p-6 border rounded-lg shadow flex flex-col w-full">
                 <h3 className="text-lg font-medium flex-grow">Photovoltaik</h3>
-                <p className="text-3xl font-bold">10,0 GWh</p>
+                <p className="text-3xl font-bold">{pvEnergy} GWh</p>
                 <p className="text-sm">+200%</p>
               </div>
             </div>
