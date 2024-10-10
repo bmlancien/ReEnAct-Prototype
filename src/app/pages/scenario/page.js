0b808f3e1db589b8b7c70523f04c03564655384f
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-10-10 13:15:46 +0100

    test for useSearchParams

---
+++ src/app/pages/scenario/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useSearchParams } from 'next/navigation';
+
+export default function ScenarioDetails() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
+
+  return (
+    <div>
+      {id === '1' && <div>Content for Item 1</div>}
+      {id === '2' && <div>Content for Item 2</div>}
+      {!id && <div>Select an item from the previous page.</div>}
+    </div>
+  );
+}
