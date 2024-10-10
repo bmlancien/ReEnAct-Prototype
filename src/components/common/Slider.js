386d23b8d1ae4fafef0e5d60a9fa1a5e2fe1f7c0
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-10-10 11:20:11 +0100

    reduce slider/switch sizes

--- src/components/common/Slider.js
+++ src/components/common/Slider.js
@@ -10,7 +10,7 @@ const Slider = ({ label, min, max, step, value, onChange, unit, goal }) => {
       {/* Slider label and current value */}
       <div className="flex justify-between items-center mb-2">
         <div className="flex items-center space-x-2">
-          <label className="font-semibold text-gray-700">{label}</label>
+          <label className="text-gray-700">{label}</label>
           <InformationCircleIcon className="h-5 w-5 text-gray-500" />
         </div>
         <span className="text-gray-700">{clampedValue} {unit}</span>
--- src/components/common/Switch.js
+++ src/components/common/Switch.js
@@ -25,12 +25,17 @@ export default function Example({ container }) {
     <Switch
       checked={enabled}
       onChange={setEnabled}
-      className="group relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 data-[checked]:bg-sky-600"
+      className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-sky-600 focus:ring-offset-2"
     >
       <span className="sr-only">Use setting</span>
+      <span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-white" />
       <span
         aria-hidden="true"
-        className="pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-5"
+        className="pointer-events-none absolute mx-auto h-4 w-9 rounded-full bg-gray-200 transition-colors duration-200 ease-in-out group-data-[checked]:bg-sky-600"
+      />
+      <span
+        aria-hidden="true"
+        className="pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out group-data-[checked]:translate-x-5"
       />
     </Switch>
   )
