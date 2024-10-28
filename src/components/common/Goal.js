6a25b4269ea64baa0aab7594525a193c0267af22
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-10-28 11:15:33 +0000

    change goal component

--- src/components/common/Goal.js
+++ src/components/common/Goal.js
@@ -1,14 +1,14 @@
-import { GlobeEuropeAfricaIcon } from '@heroicons/react/24/outline';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
 const Goal = () => {
   return (
     <div className="flex items-center border-2 border-sky-300 py-1 px-2 text-sky-800">
-      <GlobeEuropeAfricaIcon 
-        className="w-6 h-6 me-2"
-      />
-      <span className="text-lg">
-        Ziel 2040: <b>0 Tonne CO<sub>2</sub></b>
+      <span className="text-lg text-sky-600">
+        Ziel 2040
       </span>
+      <InformationCircleIcon 
+        className="w-6 h-6 ms-2 text-sky-600"
+      />
     </div>
   )
 }
